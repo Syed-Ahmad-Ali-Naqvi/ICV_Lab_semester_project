40e31138c48980c948674ff83311bd5db75bc00c
Syed Ahmad Ali Naqvi <syedahmadalinaqvi05@gmail.com>
2025-06-10 22:58:06 +0500

    Initial Input of images complete, setting up of motiion detection next

--- app.py
+++ app.py
@@ -2,12 +2,15 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
-from fastapi import Request
+from fastapi import Request, File, UploadFile
+from fastapi.responses import JSONResponse
 from fastapi.templating import Jinja2Templates
+from motiondetector import *
 
 app = FastAPI()
+
 templates = Jinja2Templates(directory="templates")
-app.mount("/static",name="static")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 app.add_middleware(
     CORSMiddleware,
@@ -20,3 +23,19 @@ app.add_middleware(
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+# Get both images
+@app.post("/submit")
+async def submit_data(image1: UploadFile = File(...), image2: UploadFile = File(...)):
+    # read bytes
+    data1 = await image1.read()
+    data2 = await image2.read()
+
+    print(f"Received files: {image1.filename}, {image2.filename}")
+
+    return JSONResponse({
+        "filename1": image1.filename,
+        "filename2": image2.filename,
+        "message": "Received both files!"
+        # …plus any results you want to send back…
+    })
