d539b5f9bb5f5f399a4a0c3deca4b1596e0d94e7
Syed Ahmad Ali Naqvi <syedahmadalinaqvi05@gmail.com>
2025-06-12 20:35:31 +0500

    Initial prototype done

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, Form
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
@@ -25,14 +25,26 @@ async def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 @app.post("/submit")
-async def submit_data(image1: UploadFile = File(...), image2: UploadFile = File(...)):
+async def submit_data(image1: UploadFile = File(...), image2: UploadFile = File(...), method_received: str = Form(...)):
+    method_map = {
+        "horn_schunck": motion_detect_horn_schunck,
+        "pyr_lucas_kanade": motion_detect_pyr_lucas_kanade,
+        "ssd": motion_detect_ssd
+    }
     data1 = await image1.read()
     data2 = await image2.read()
 
-    print(f"Received files: {image1.filename}, {image2.filename}")
+    arr1 = np.frombuffer(data1, np.uint8)
+    arr2 = np.frombuffer(data2, np.uint8)
+    frame1 = cv2.imdecode(arr1, cv2.IMREAD_COLOR)
+    frame2 = cv2.imdecode(arr2, cv2.IMREAD_COLOR)
 
-    return JSONResponse({
-        "filename1": image1.filename,
-        "filename2": image2.filename,
-        "message": "Received both files!"
-    })
+    if method_received not in method_map:
+        return Response(status_code=400, content=b"Unknown method")
+    result_img = method_map[method_received](cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY), cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY))
+
+    success, buffer = cv2.imencode('.png', result_img)
+    if not success:
+        return Response(status_code=500, content=b"Encoding failed")
+
+    return Response(content=buffer.tobytes(), media_type="image/png")
